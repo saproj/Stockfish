22c7d8f7e4e2b8d7f82810dfdb8dbd2319a16be0
saproj <saproj@gmail.com>
2017-05-31 12:29:00 +0200

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -208,7 +208,7 @@ namespace {
   const int RookCheck   = 880;
   const int BishopCheck = 435;
   const int KnightCheck = 790;
-  const int OtherCheck  = 155;
+  const int OtherCheck  = 160;
 
   // Threshold for lazy and space evaluation
   const Value LazyThreshold  = Value(1500);
